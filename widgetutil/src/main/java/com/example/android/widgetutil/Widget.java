7edfc14fc88fc7d6d955399fadcc72a6e310be0d
Sunil <hullursunil@gmail.com>
2019-03-25 18:15:48 +0530

    Create Widget Repo.

---
+++ widgetutil/src/main/java/com/example/android/widgetutil/Widget.java
@@ -0,0 +1,13 @@
+package com.example.android.widgetutil;
+
+import android.util.Log;
+
+/**
+ * Created by Sunil on 25-03-2019.
+ */
+
+public class Widget {
+    public static void start(String msg){
+        Log.d("msg",msg);
+    }
+}
